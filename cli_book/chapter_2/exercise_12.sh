7743ef9841a7c07ec427efac09094ae924067a7b
Mahsa <mahskh08@gmail.com>
2023-07-07 20:04:23 +0430

    exercise completed

---
+++ cli_book/chapter_2/exercise_12.sh
@@ -0,0 +1,10 @@
+// View what you have done with the tree command: tree ~/cli-tmp.
+$ tree ~/cli-tmp
+/Users/[username]/cli-tmp/
+└── parents
+  ├── children
+  │   ├── bob
+  │   └── grandchildren
+  │       └── bob
+  └── nephews
+      └── bob
