054f83e5f620e450e05e958bf66e93f491c03a4a
Mahsa <mahskh08@gmail.com>
2023-07-12 10:46:48 +0430

    exercise completed

---
+++ cli_book/chapter_4/exercise_5.sh
@@ -0,0 +1,7 @@
+# What are mary's permissions to the laptop file in the following example? What are the permissions for the group acmeinc?
+$ ls -l vip_lounge
+...
+-r-xrwx---  1 mary acmeinc 0 Jul 21 17:57 laptop
+...
+
+# The use mary has the read and execute access to the laptop file, and the group acmeinc has read, write and execute permissions.
