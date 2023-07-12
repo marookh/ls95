45e44943a452cc84844e98af451ca5d70e8786f0
Mahsa <mahskh08@gmail.com>
2023-07-12 10:43:40 +0430

    exercise completed

---
+++ cli_book/chapter_4/exercise_4.sh
@@ -0,0 +1,10 @@
+# What are mary's permissions to the donuts file in the following example:
+$ groups
+mary travelers vip
+$ ls -l vip_lounge
+$ ls -l ./
+...
+-rw-rw----  1 acmeair  vip  0 Jul 21 17:57 donuts
+...
+# The user mary has the read and write access to the dnuts file.
+
