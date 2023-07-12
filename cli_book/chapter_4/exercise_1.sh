18c2c7f6abd25959132bc0efa4381dcb762b3db8
Mahsa <mahskh08@gmail.com>
2023-07-12 10:26:34 +0430

    exercise completed

---
+++ cli_book/chapter_4/exercise_1.sh
@@ -0,0 +1,6 @@
+# Which user and group are assigned to the /etc folder on your computer?
+# Ypu can find the user and group of the /etc folder by running the following command:
+
+$ ls -l /
+
+# The above command will list multiple files and directories, so just find the one for /etc. It should have the user root and probably the group root if you are on linux. If you run the same command on a Mac computer, teh group for the /etc folder probably wheel.
