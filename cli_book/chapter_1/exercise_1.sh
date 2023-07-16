f4e94fd8d22aaf62b4b4d4ebdd90478ee8787b69
Mahsa <mahskh08@gmail.com>
2023-07-16 22:18:03 +0430

    comments added

--- cli_book/chapter_1/exercise_1.sh
+++ cli_book/chapter_1/exercise_1.sh
@@ -1,5 +1,4 @@
-/*
-The echo command has only one optional flag: -n, which means "Do not print the trailing newline character." Experiment with echo. Run the following commands, guessing before running each what the output will be:
+#The echo command has only one optional flag: -n, which means "Do not print the trailing newline character." Experiment with echo. Run the following commands, guessing before running each what the output will be:
 
 echo "hello world"
 echo hello world
@@ -7,9 +6,9 @@ echo -n hello world
 echo hello world -n
 echo "hello world" -n
 echo "-n hello world"
-*/
 
-//The following is the expected output of the above commands:
+
+#The following is the expected output of the above commands:
 
 $ echo "hello world"
 hello world
@@ -23,12 +22,12 @@ hello world -n
 $ echo "-n hello world"
 -n hello world
 
-/*
-The above commands illustrate the following ideas:
 
-1.Arguments can be wrapped in quotes, but don't always have to be wrapped.
-2.Flags (like -n) usually must come before other arguments. (There are exceptions to this rule.)
-3.Misplaced arguments and/or flags change the output.
-Also note that on line 6, in Bash, the prompt stayed on the same line as the output. This isn't a typo. It's the result of the -n flag being used properly.
-*/
+#The above commands illustrate the following ideas:
+
+#1.Arguments can be wrapped in quotes, but don't always have to be wrapped.
+#2.Flags (like -n) usually must come before other arguments. (There are exceptions to this rule.)
+#3.Misplaced arguments and/or flags change the output.
+#Also note that on line 6, in Bash, the prompt stayed on the same line as the output. This isn't a typo. It's the result of the -n flag being used properly.
+
 
