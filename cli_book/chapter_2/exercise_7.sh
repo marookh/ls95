091975aff4738cda04ea4393b1c6efad93d0bdc5
Mahsa <mahskh08@gmail.com>
2023-07-17 12:17:39 +0430

    exercise completed

--- cli_book/chapter_2/exercise_7.sh
+++ cli_book/chapter_2/exercise_7.sh
@@ -1,3 +1,3 @@
-// Create a nested set of directories in your home directory: cli-tmp > parents > children > grandchildren (hint: use the -p flag to do it all at once).
+#Create a nested set of directories in your home directory: cli-tmp > parents > children > grandchildren (hint: use the -p flag to do it all at once).
 
 $ mkdir -p ~/cli-tmp/parents/children/grandchildren
