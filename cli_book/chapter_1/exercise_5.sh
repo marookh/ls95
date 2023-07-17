c759909019b43474d923a1f28f152f63cd028664
Mahsa <mahskh08@gmail.com>
2023-07-17 12:08:55 +0430

    issue fixed

--- cli_book/chapter_1/exercise_5.sh
+++ cli_book/chapter_1/exercise_5.sh
@@ -1,6 +1,5 @@
-/*
-You can get more information about what a command does, how it works, and which flags you can use by refrencing the manual for that command. To read the manual pages for a command, use the man command:
-*/
+#You can get more information about what a command does, how it works, and which flags you can use by refrencing the manual for that command. To read the manual pages for a command, use the man command:
+
 
 $ man touch
 TOUCH(1)                    User Commands                   TOUCH(1)
@@ -17,10 +16,11 @@ DESCRIPTION
        Update  the access and modification times of each FILE to the
        current time.
 ...
-/*
-The "SYNOPSIS" section of some manual pages describes the available flags that can be used. In the case of the touch command, the flags can be used in any order just after the touch command itself, and the file is the last argument. The possible options are described in the "DESCRIPTION" section.
 
-You may have noticed that the man command uses less to display the content of the manpages, so you can use the up- and down-arrow keys to navigate the contents of the manual.
+#The "SYNOPSIS" section of some manual pages describes the available flags that can be used. In the case of the touch command, the flags can be used in any order just after the touch command itself, and the file is the last argument. The possible options are described in the "DESCRIPTION" section.
 
-To exit man, you can type the q key.
-*/
+
+#You may have noticed that the man command uses less to display the content of the manpages, so you can use the up- and down-arrow keys to navigate the contents of the manual.
+
+#To exit man, you can type the q key.
+
