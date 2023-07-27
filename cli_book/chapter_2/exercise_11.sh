f792633efe2dacab8f683c08db05c0e3d605b71a
Mahsa <mahskh08@gmail.com>
2023-07-27 21:48:42 +0430

    add changes to the file

--- cli_book/chapter_2/exercise_11.sh
+++ cli_book/chapter_2/exercise_11.sh
@@ -1,3 +1,3 @@
-// Copy the grandchildren directory to the parents directory with a new name: nephews.
+# Copy the grandchildren directory to the parents directory with a new name: nephews.
 
 $ cp -r grandchildren ../nephews
