55337c3d7814c52aa6598106d08e52968c7e8d5c
Mahsa <mahskh08@gmail.com>
2023-08-03 11:21:47 +0430

    add some changes to the file

--- cli_book/chapter_2/exercise_17.sh
+++ cli_book/chapter_2/exercise_17.sh
@@ -1,9 +1,9 @@
-// List the files in the parent directory. Then list the files in the parent directory's parent direcotry.
+# List the files in the parent directory. Then list the files in the parent directory's parent direcotry.
 
 $ ls ..
 ubunto
 
-// What happens if you run that command from the root directory?
+# What happens if you run that command from the root directory?
 
 $ cd /
 $ ls ..
@@ -12,9 +12,9 @@ boot  initrd.img      lost+found  proc  srv   var
 dev   initrd.img.old  media       root  sys   vmlinuz
 etc   lib             mnt         run   tmp   vmlinuz.old
 
-/*
-As you can see, since the root directory doesn't have a parent directory, it just prints out the files in the root directory itself.
-To list the files of the parent's parent directory, just add a slash and an extra ..:
+
+#As you can see, since the root directory doesn't have a parent directory, it just prints out the files in the root directory itself.
+#To list the files of the parent's parent directory, just add a slash and an extra ..:
 
 $ cd
 $ ls ../..
