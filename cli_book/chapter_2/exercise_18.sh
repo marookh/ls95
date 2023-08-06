a6994ae217ff802953b3f7a8ad8294d0a939b648
Mahsa <mahskh08@gmail.com>
2023-08-06 19:01:01 +0430

    add some changes

--- cli_book/chapter_2/exercise_18.sh
+++ cli_book/chapter_2/exercise_18.sh
@@ -1,3 +1,3 @@
-// What does ../abc mean?
+# What does ../abc mean?
 
-The ../abc means there is a directory within the parent directory called "abc" which is in the same level with current directory.
+# The ../abc means there is a directory within the parent directory called "abc" which is in the same level with current directory.
